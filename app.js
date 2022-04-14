95e7ff9ed7ee7489f0e15711525b4ccc03e9a84e
xDamneDx <xdamnedx@gmail.com>
2022-04-14 16:58:57 +0300

    Added functionality for adding todos

---
+++ app.js
@@ -0,0 +1,25 @@
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
+
+const generateTemplateTodo = (todo) => {
+  const html = `
+    <li
+        class="list-group-item text-light d-flex justify-content-between align-items-center"
+    >
+        <span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>
+  `;
+
+  list.innerHTML += html;
+};
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+
+  if (todo) {
+    generateTemplateTodo(todo);
+    addForm.reset();
+  }
+});
